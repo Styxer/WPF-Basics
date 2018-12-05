ba67518ef0dccde53ed2dd5dec30a78209e6083a
ofir <gennin@gmail.com>
2018-12-05 20:52:08 +0200

    Some basic controls

--- WPF-Basics/WPF-Basics/MainWindow.xaml.cs
+++ WPF-Basics/WPF-Basics/MainWindow.xaml.cs
@@ -24,5 +24,63 @@ namespace WPF_Basics
         {
             InitializeComponent();
         }
+
+        private void ApplyBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show( $"The Description is  { DescriptionTxtBox.Text }" );
+        }
+
+        private void ResetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            WeldCheckBox.IsChecked = AssemblyCheckBox.IsChecked = PlasmaCheckBox.IsChecked = LaserCheckBox.IsChecked = PurchaseCheckBox.IsChecked =
+                LaserCheckBox.IsChecked = DrillCheckBox.IsChecked = FoldCheckBox.IsChecked = RollCheckBox.IsChecked = SawCheckBox.IsChecked = false;
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            LenghtCheckBox.Text += ((CheckBox)sender).Content + " ";
+            
+               
+
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            var checkBoxTxt = ((CheckBox)sender).Content.ToString();
+            LenghtCheckBox.Text = LenghtCheckBox.Text.Replace(checkBoxTxt, String.Empty); 
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (NoteTextBox != null)
+            {
+                var comboBox = (ComboBox)sender;
+                var value = ((ComboBoxItem)comboBox.SelectedValue).Content.ToString();
+
+                NoteTextBox.Text = value;
+            }
+        
+
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ComboBox_SelectionChanged(FinishComboBox, null);
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            MassTextBox.Text = SupplierNameTextBox.Text;
+            
+        }
+
+        private void GoToTreeViewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            TreeView treeViewWindows = new TreeView();
+            treeViewWindows.ShowDialog();
+        }
     }
 }
